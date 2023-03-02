0432f867f9f0135178e23975596ed6bdf99d7ddd
unqtd <kxyzd@vk.com>
2023-03-02 22:25:17 +0300

    Убраны не оправдавшие себя вещи

--- digital.c
+++ digital.c
@@ -7,10 +7,8 @@ enum { HIGH = 1, LOW = 0 };
 
 inline bool digital_read(pin_t pin) {
   port_t const *port = _get_port(pin);
-  #ifndef DISABLE_CHECKING
   if (port == NULL)
     return LOW;
-  #endif
 
   // return bit_is_set(*port->pin, _get_port_pin(pin));
   return *port->pin & bit(_get_port_pin(pin));
@@ -18,10 +16,8 @@ inline bool digital_read(pin_t pin) {
 
 inline void digital_write(pin_t pin, const bool value) {
   port_t const *port = _get_port(pin);
-  #ifndef DISABLE_CHECKING
   if (port == NULL)
     return;
-  #endif
 
   if (value)
     *port->port |= bit(_get_port_pin(pin));
@@ -29,19 +25,11 @@ inline void digital_write(pin_t pin, const bool value) {
     *port->port &= ~bit(_get_port_pin(pin));
 }
 
-#ifdef DYN
-void dyn_digital_write(pin_t pin, const bool value) {
-  digital_write(pin, value);
-}
-#endif
-
 inline void toggle(pin_t pin) {
   port_t const *port = _get_port(pin);
 
-  #ifndef DISABLE_CHECKING
   if (port == NULL)
     return;
-  #endif
 
   *port->port ^= bit(_get_port_pin(pin));
 }
